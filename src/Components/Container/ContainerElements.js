0d6e428c4adcda9e8c1c2290bd5d5fbc86b269ad
solayman <mdsolaymancse@gmail.com>
2022-07-04 22:55:56 +0600

    design

--- src/Components/Container/ContainerElements.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  margin: 0px 350px;
-
-  @media (min-width: 320px) and (max-width: 480px) {
-    margin: auto;
-  }
-`;
--- src/Components/NurseArea/NurseAreaElements.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Wrapper = styled.div`
-    width: 100%;
-    padding: 50px 0px;
-    
-    
-
-`;
